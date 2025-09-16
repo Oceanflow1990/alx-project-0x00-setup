5f8a3ffab371be7f6c07e171b9b032f3a8a788ea
Opeyemi <eco.ajibade@gmail.com>
2025-09-16 19:32:41 +0100

    alx-project-0x00-setup

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ButtonProps } from "../interfaces";
+import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, style }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button
-      className={`px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 transition ${style}`}
+      className={`px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}
     >
       {title}
     </button>
